ee9d2125c5a7d95925b409c31ae761aa9ba01d4e
wilson <kengnewilson63@gmail.com>
2026-04-14 08:24:58 +0100

    Ajouter les routes et le composant Contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,22 @@ import { Routes } from '@angular/router';
 import { Main } from './main/main';
 import { About } from './about/about';
 import { Service } from './service/service';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [{
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component:Main
+    component: Main
   },
   {
     path: 'about',
-component:About
+    component: About
   },
   {
-    path: 'services',
-   component:Service
-   
+    path: 'service',
+    component: Service
+  },
+  {
+    path: 'contact',
+    component: Contact
   }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "./footer/footer";
-import { Main } from "./main/main";
 import { Header } from "./header/header";
-import { About } from "./about/about";
-import { Service } from "./service/service";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Main, Header, About, Service],
+  imports: [RouterOutlet, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
